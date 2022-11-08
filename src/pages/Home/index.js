a4b6e172abc85d9a247ea868c86a93a80a5fac67
DevJuniorBruno <brunode2001@gmail.com>
2022-11-07 21:31:06 -0300

    working update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,39 @@
 import './home.css';
+import { Link } from 'react-router-dom';
+import Caminhao from '../../img/caminhao.png'
+import Localizacao from '../../img/localizacao.png'
 
 function Home() {
     return(
         <div className='wraper'>
             <main>
                 <section className='module parallax parallax-1'>  
-                    <h1>Dev junior bruno</h1>
+                    <h1 className='saibamais'>Clínica Veterinária Simplesvet </h1>
+                    <div className='divButton'>
+                        <Link className='fourth' to='clinica'>Saiba mais</Link>
+                    </div>
                 </section>
 
                 <section className='module content'>
-                    <div className='container'>
-                        <h2>Qalquer Texto Aqui</h2>
+                    <div className='icones-links'>
+                    <Link to="clinica" >
+                        <img className='caminhao' to='clinica' src={ Caminhao } alt="caminhao-link" />
+                        <p>Táxi Dog</p>
+                    </Link> 
+                    </div>
+                    <div className='icones-links'>
+                    <Link to="localizacao" >
+                        <img className='localizacao' to='localizacao' src={ Localizacao } alt="caminhao-link" />
+                        <p>Táxi Dog</p>
+                    </Link> 
+                    </div>
+                    <div className='icones-links'></div>
+                    <div className='icones-links'></div>
+                </section>
+
+                <section className='module info'>
+                   <div className='container-infos'>
+                    <h2>Qalquer Texto Aqui</h2>
                         <p>
                             lorem imsasdasdasda ddasdsad sdasdadasdas asdmoackmascka scasmcsakmcsa aslckmaslsak
                         </p>
@@ -18,7 +41,7 @@ function Home() {
                             lorem imsasdasdasda ddasdsad sdasdadasdas asdmoackmascka scasmcsakmcsa aslckmaslsak
                         </p>
                     </div>
-                </section>
+                   </section>
 
             </main>
         </div>
