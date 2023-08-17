fa01fb9314879ffd22fc2b9b8a4139b9f0dddb7e
DevJuniorBruno <brunode2001@gmail.com>
2023-08-17 15:39:00 -0300

    rm  bug hamburguer

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -6,20 +6,21 @@ import logo from '../../img/logo.png';
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
   const navLinksRef = useRef(null);
-  const location = useLocation()
+  const location = useLocation();
 
   useEffect(() => {
-    // Função que será chamada quando o usuário clicar fora do componente
     const handleClickOutside = (event) => {
-      if (navLinksRef.current && !navLinksRef.current.contains(event.target)) {
+      if (
+        navLinksRef.current &&
+        !navLinksRef.current.contains(event.target) &&
+        !event.target.classList.contains('nav_toggle')
+      ) {
         setIsOpen(false);
       }
     };
 
-    // Adiciona o event listener para detectar os cliques fora do componente
     document.addEventListener('mousedown', handleClickOutside);
 
-    // Remove o event listener ao desmontar o componente
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
@@ -29,6 +30,10 @@ function Header() {
     setIsOpen(false);
   }, [location]);
 
+  const handleToggleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="navbar">
       <span>
@@ -37,7 +42,7 @@ function Header() {
         </Link>
       </span>
       <div className={`nav_links ${isOpen && 'open'}`} ref={navLinksRef}>
-        <Link to="/">Home</Link>
+      <Link to="/">Home</Link>
         <Link to="clinica">Clínica</Link>
         <Link to="servicos">Farmácia e Pet Shop</Link>
         <Link to="banhoetosa">Banho e Tosa</Link>
@@ -46,7 +51,7 @@ function Header() {
       </div>
       <div
         className={`nav_toggle ${isOpen && 'open'}`}
-        onClick={() => setIsOpen(!isOpen)}
+        onClick={handleToggleClick}
       >
         <div className="bar"></div>
       </div>
